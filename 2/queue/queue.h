957f552626e636005546d0fe0a7df5f9f64dd03d
caixukun <2468085704@qq.com>
2015-10-18 19:48:39 +0800

    d

--- 2/queue/queue.h
+++ 2/queue/queue.h
@@ -9,7 +9,17 @@ struct QueueRecord{
     int Front;
     int Rear;
     int Size;
-    ElementType *
+    ElementType *Array;
 };
+
+int IsEmpty(Queue Q);
+int IsFull(Queue Q);
+Queue CreateQueue(int MaxElements);
+void DisposeQueue(Queue Q);
+void MakeEmpty(Queue Q);
+void Enqueue(ElementType X, Queue Q);
+ElementType Front(Queue Q);
+void Dequeue(Queue Q);
+ElementType FrontAndDequeue(Queue Q);
 
 #endif
