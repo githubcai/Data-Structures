b26ced58058194deb6031974bda1adbc7b12f38d
caixukun <2468085704@qq.com>
2015-10-23 18:44:15 +0800

    d

--- 4/tree.c
+++ 4/tree.c
@@ -60,3 +60,30 @@ SearchTree Insert(ElementType X, SearchTree T){
 
     return T;
 }
+
+SearchTree Delete(ElementType X, SearchTree T){
+    Position TmpCell;
+    if(T==NULL){
+        FatalError("Element not found");
+    }else if(X<T->Element){
+        T->Left = Delete(X, T->Left);
+    }else if(X > T->Element){
+        T->Right = Delete(X, T->Right);
+    }else if(T->Left && T->Right){
+        TmpCell = FindMin(T->Right);
+        T->Element = TmpCell->Element;
+        T->Right = Delete(T->Element, T->Right);
+    }else{
+        TmpCell = T;
+        if(T->Left==NULL){
+            T = T->Right;
+        }else if(T->Right==NULL){
+            T = T->Left;
+        }
+        free(TmpCell);
+    }
+}
+
+ElementType Retrieve(Position P){
+    return P->Element;
+}
